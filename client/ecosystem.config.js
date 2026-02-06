f6d9e07c416cb985c408e902a9e2239f9582a9a0
Sahil <49190338+sahilkumar028@users.noreply.github.com>
2026-02-06 12:42:33 +0530

    Update API URL in ecosystem configuration

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         script: "serve",
         args: "-s build -l 3000",
         env: {
-          REACT_APP_API_URL: "https://api.lrecoolingservice.com",
+          REACT_APP_API_URL: "https://sahilapi.sahaedu.in",
           NODE_ENV: "production",
         },
       },
